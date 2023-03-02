c6b317c15ae49dbf1db72daa9f0c7d5d56bb3cf8
Alan Lengkoan <alanlengkoan15@gmail.com>
2023-03-02 13:51:58 +0800

    update again

--- si_jemaat_gereja/src/Controller/SecurityController.php
+++ si_jemaat_gereja/src/Controller/SecurityController.php
@@ -24,7 +24,7 @@ class SecurityController extends AbstractController
         $this->mng   = $mng;
         $this->myfun = $myfun;
     }
-    
+
     /**
      * @Route("/login", name="app_login")
      */
